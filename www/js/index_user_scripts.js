bd05354add4184e094902ef326681ea5b8c50553
wilsongat2 <wilsongat2@yahoo.com.br>
2017-04-20 10:32:20 -0300

    Add files via upload

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,417 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  .uib_w_3 */
+    $(document).on("click", ".uib_w_3", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_6 */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Cadastro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_7 */
+    
+    
+        /* button  .uib_w_8 */
+    $(document).on("click", ".uib_w_8", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#CRM"); 
+         return false;
+    });
+    
+        /* button  .uib_w_9 */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Consulta"); 
+         return false;
+    });
+    
+        /* button  .uib_w_10 */
+    $(document).on("click", ".uib_w_10", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Consulta"); 
+         return false;
+    });
+    
+        /* button  .uib_w_17 */
+    $(document).on("click", ".uib_w_17", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_22 */
+    $(document).on("click", ".uib_w_22", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_26 */
+    $(document).on("click", ".uib_w_26", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_30 */
+    $(document).on("click", ".uib_w_30", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_30 */
+    $(document).on("click", ".uib_w_30", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_31 */
+    
+    
+        /* button  .uib_w_49 */
+    
+    
+        /* button  .uib_w_48 */
+    $(document).on("click", ".uib_w_48", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_7 */
+    
+    
+        /* button  .uib_w_7 */
+    $(document).on("click", ".uib_w_7", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  Janeiro */
+    $(document).on("click", ".uib_w_33", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#janeiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_31 */
+    $(document).on("click", ".uib_w_31", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_49 */
+    $(document).on("click", ".uib_w_49", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_54 */
+    $(document).on("click", ".uib_w_54", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_53 */
+    $(document).on("click", ".uib_w_53", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_55 */
+    $(document).on("click", ".uib_w_55", function(evt)
+    {
+        /* your code goes here */ 
+         return false;
+    });
+    
+        /* button  .uib_w_57 */
+    $(document).on("click", ".uib_w_57", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_56 */
+    $(document).on("click", ".uib_w_56", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  Fevereiro */
+    $(document).on("click", ".uib_w_34", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Feb2"); 
+         return false;
+    });
+    
+        /* button  Março */
+    $(document).on("click", ".uib_w_35", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#marco"); 
+         return false;
+    });
+    
+        /* button  Maio */
+    $(document).on("click", ".uib_w_37", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#maio"); 
+         return false;
+    });
+    
+        /* button  Junho */
+    $(document).on("click", ".uib_w_38", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#junho"); 
+         return false;
+    });
+    
+        /* button  Julho */
+    $(document).on("click", ".uib_w_39", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#julho"); 
+         return false;
+    });
+    
+        /* button  Agosto */
+    $(document).on("click", ".uib_w_40", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#agosto"); 
+         return false;
+    });
+    
+        /* button  Setembro */
+    $(document).on("click", ".uib_w_41", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#setembro"); 
+         return false;
+    });
+    
+        /* button  Abril */
+    $(document).on("click", ".uib_w_36", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#abril"); 
+         return false;
+    });
+    
+        /* button  Outubro */
+    $(document).on("click", ".uib_w_43", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#outubro"); 
+         return false;
+    });
+    
+        /* button  Novembro */
+    $(document).on("click", ".uib_w_42", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#novembro"); 
+         return false;
+    });
+    
+        /* button  Dezembro */
+    $(document).on("click", ".uib_w_44", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#dezembro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_67 */
+    $(document).on("click", ".uib_w_67", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_65 */
+    $(document).on("click", ".uib_w_65", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_72 */
+    $(document).on("click", ".uib_w_72", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_70 */
+    $(document).on("click", ".uib_w_70", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_75 */
+    $(document).on("click", ".uib_w_75", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_77 */
+    $(document).on("click", ".uib_w_77", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_82 */
+    $(document).on("click", ".uib_w_82", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_80 */
+    $(document).on("click", ".uib_w_80", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_97 */
+    $(document).on("click", ".uib_w_97", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_95 */
+    $(document).on("click", ".uib_w_95", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_101 */
+    $(document).on("click", ".uib_w_101", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_100 */
+    $(document).on("click", ".uib_w_100", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_92 */
+    $(document).on("click", ".uib_w_92", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_90 */
+    $(document).on("click", ".uib_w_90", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_85 */
+    $(document).on("click", ".uib_w_85", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_87 */
+    $(document).on("click", ".uib_w_87", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+        /* button  .uib_w_60 */
+    $(document).on("click", ".uib_w_60", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_62 */
+    $(document).on("click", ".uib_w_62", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#Fevereiro"); 
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
